f21346301d1374f63241f2b61f94a672fc5f678d
Dustin Myers <dustinblaine.myers@gmail.com>
2020-01-24 17:25:43 -0500

    obstacles appear on screen the way I want

--- jumperguy.py
+++ jumperguy.py
@@ -1,4 +1,16 @@
 import pygame
+import sys
+import random
+pygame.font.init()
+from titlescreen import titlescreen
+from exitscreen import exitscreen
+
+# Calling the title screen
+
+lastloc = 1000
+
+titlescreen()
+
 
 def main():
 
@@ -6,28 +18,87 @@ def main():
 
     background_one = pygame.image.load('background.png')
     background_two = pygame.image.load('background2.png')
-    # background_three = pygame.image.load('background3.png')
 
     background_one_x = 0
     background_two_x = background_one.get_width()
-    # background_three_x = background_two.get_width()
 
     # Setting window display size
 
-    width = background_one.get_width()
-    height = background_one.get_height()
-    screen = pygame.display.set_mode((width, height))
+    WIDTH = 1000
+    HEIGHT = 350
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    
 
+    # define colors
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
+
+    
     # Initiliazing pygame, setting game caption, and initializing clock.
     
     pygame.init()
     pygame.display.set_caption('Jumper guy')
     clock = pygame.time.Clock()
 
-    #Setting the speed of fps limit, and speed of the background, bigger numbers mean faster speed.
+    #frame tracker to randomly put obstacles on screen
+    frame_tracker = 0
 
-    fps = 30 
+    # Setting the speed of FPS limit, and speed of the background, bigger numbers mean faster speed.
+    
+    FPS = 60
     speed = 10
+    
+    # Setting score variable and font
+
+    myfont = pygame.font.Font('8bit16.ttf', 28)
+    
+    real_score = 0
+    score = 0
+
+    class Mob(pygame.sprite.Sprite):
+       def __init__(self, x_location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = x_location
+        self.rect.y = 250
+        self.speedy = 0
+        self.speedx = -10
+        
+        # lastloc = 1000
+        
+       def update(self):
+           self.rect.x += self.speedx
+           
+           
+        
+           if self.rect.x < -25  :
+            obstacle_generation(self)
+       
+            
+            
+        #    if lastloc > 3000:
+        #        lastloc = 1000 
+    def obstacle_generation(obstacle):
+           global lastloc
+           obstacle.rect.x = lastloc + 100
+           obstacle.rect.y = 250
+           lastloc = obstacle.rect.x
+           print(obstacle.rect.x)
+           
+    all_sprites = pygame.sprite.Group()
+    mobs = pygame.sprite.Group()
+
+    for i in range(1):
+        m = Mob(lastloc + random.randrange(1500))
+        all_sprites.add(m)
+        mobs.add(m)
+        
 
     # Game initialization and starting of main loop
 
@@ -43,29 +114,49 @@ def main():
 
         # Game logic
 
+        #Logic to randomly place obstacles
+
+        if frame_tracker % 300 == 0:
+            for i in range(1):
+                m = Mob(lastloc + random.randrange(1500))
+                all_sprites.add(m)
+                mobs.add(m)
+
         # Drawing the moving background
         
         screen.blit(background_one, [background_one_x, 0])
         screen.blit(background_two, [background_two_x, 0])
-        # screen.blit(background_three, [background_three_x, 0])
         
         background_one_x -= speed
         background_two_x -= speed
-        # background_three_x -= speed
 
         if background_one_x <= -1 * background_one.get_width():
             background_one_x = background_two_x + background_two.get_width()
         if background_two_x <= -1 * background_two.get_width():
-            background_two_x = background_one_x + background_one.get_width()
-        # if background_three_x <= -1 * background_three.get_width():
-        #     background_three_x = background_two_x + background_two.get_width()
+            background_two_x = background_one_x + background_one.get_width() + 1
 
+        # Adding to the score and displaying it.
+        real_score += 1
+        score_text = myfont.render('Score = {}'.format(score), 1, (255,255,255))
+        screen.blit(score_text, [10,320])
+
+        if real_score % 10 == 0:
+            score +=1
+
+        all_sprites.update() 
+        all_sprites.draw(screen)
+        pygame.display.flip()   
         pygame.display.update()
+        frame_tracker += 1
         
         # Game clock
-        clock.tick(fps)
+        clock.tick(FPS)
+         
+    exitscreen()
 
     pygame.quit()
+
+    
 
 if __name__ == '__main__':
     main()
